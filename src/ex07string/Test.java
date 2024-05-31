ec82a3fc93211452337ac1f5d9f4d7d458452155
laroro <dark42@naver.com>
2024-05-31 17:59:08 +0900

    집에서한거

--- src/ex07string/Test.java
+++ src/ex07string/Test.java
@@ -2,8 +2,9 @@ package ex07string;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("1.학원에서푸시");
 		
+		System.out.println("1.학원에서푸시");
+		System.out.println("2.집에서복습");
 		System.out.println("3.학원에서 수업");
 	}
 }
